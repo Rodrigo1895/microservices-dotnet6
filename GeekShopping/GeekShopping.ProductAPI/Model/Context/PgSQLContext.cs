d23adcf15d394b65ba74f40ff737a473d1e552cb
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2022-01-27 23:05:24 -0300

    Commit Inicial

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/PgSQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class PgSQLContext : DbContext
+    {
+        public PgSQLContext()
+        {}
+
+        public PgSQLContext(DbContextOptions<PgSQLContext> options) : base(options)
+        {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
