ecdac056e44219ade6dcd1dfd6104c4115309fae
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2022-01-27 23:33:41 -0300

    Seed DB

--- GeekShopping/GeekShopping.ProductAPI/Migrations/20220128023013_SeedProductDataTable.Designer.cs
+++ GeekShopping/GeekShopping.ProductAPI/Migrations/20220128023013_SeedProductDataTable.Designer.cs
@@ -2,6 +2,7 @@
 using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GeekShopping.ProductAPI.Migrations
 {
     [DbContext(typeof(PgSQLContext))]
-    partial class PgSQLContextModelSnapshot : ModelSnapshot
+    [Migration("20220128023013_SeedProductDataTable")]
+    partial class SeedProductDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
